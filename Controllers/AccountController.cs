99ab1f5e0d7aab8ae3aa4ff8a5136fbcbddc15fb
loverhina <tirao.rhina@gmail.com>
2024-10-01 22:38:33 +0800

    csnmvc

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSN_Parents_Portal.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Enroll()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        public IActionResult Request()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PreRegController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSN_Parents_Portal.Controllers
+{
+    public class PreRegController : Controller
+    {
+        public IActionResult Prereg()
+        {
+            return View();
+        }
+    }
+}
